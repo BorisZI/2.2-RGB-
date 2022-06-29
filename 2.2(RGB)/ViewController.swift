5db64cbf67658145465060a7fb1f186f6d551aa0
Boris Z <Boris Z>
2022-06-29 14:54:49 +0300

    Initial Commit

---
+++ 2.2(RGB)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.2(RGB)
+//
+//  Created by Boris Alexandrov on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
