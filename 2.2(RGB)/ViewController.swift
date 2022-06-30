4cec410e92fb529b0d6a0c66698c6bc39bff9068
Boris Z <Boris Z>
2022-06-30 13:15:58 +0300

    Finish

--- 2.2(RGB)/ViewController.swift
+++ 2.2(RGB)/ViewController.swift
@@ -2,18 +2,64 @@
 //  ViewController.swift
 //  2.2(RGB)
 //
-//  Created by Boris Alexandrov on 29.06.2022.
+//  Created by Boris Zitsermsn on 29.06.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewBoard: UIView!
+    
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var redLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewBoard.layer.cornerRadius = 20
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        viewBoard.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
-
-
+    private func textForLabel (from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    @IBAction func redBalance() {
+        redLabel.text = textForLabel(from: redSlider)
+    }
+    
+    @IBAction func greenBalance() {
+        greenLabel.text = textForLabel(from: greenSlider)
+    }
+    
+    @IBAction func blueBalace() {
+        blueLabel.text = textForLabel(from: blueSlider)
+    }
+    @IBAction func rgbView(_ sender: UISlider) {
+        viewBoard.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue:CGFloat(blueSlider.value),
+            alpha: 1
+            )
+    }
 }
+    
+
+
 
